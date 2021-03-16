88029d7d8d8dd88d7e66b7b61bbdb5971a14af34
hatamjafarov <hatam.jafarov@gmail.com>
2021-03-16 21:37:32 +0400

    outputting dynamically

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,8 +1,8 @@
 import React from "react";
-import classes from "./BurgerIngredients.module.css";
+import classes from "./BurgerIngredient.module.css";
 import PropTypes from 'prop-types';
 
-const burgerIngredient = (props) => {
+const BurgerIngredient = (props) => {
     let ingredient = null;
 
     switch (props.type) {
@@ -36,8 +36,8 @@ const burgerIngredient = (props) => {
     return ingredient;
 };
 
-burgerIngredient.propTypes ={
+BurgerIngredient.propTypes ={
     type: PropTypes.string.isRequired
 }
 
-export default burgerIngredient
+export default BurgerIngredient
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger'
 
 import Auxilary from "../../hoc/Auxilary";
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
+    }
+
+
     render(){
         return(
             <Auxilary>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auxilary>
         )
